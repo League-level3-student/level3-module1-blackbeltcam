beec99d896d6e2b1eb70816f6ac29146d588846f
blackbeltcam <league@WTS14.attlocal.net>
2019-04-19 16:58:25 -0700

    TextUndoRedo almost done

--- src/_03_IntroToStacks/_02_TextUndoRedo.java
+++ src/_03_IntroToStacks/_02_TextUndoRedo.java
@@ -4,52 +4,77 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.Stack;
 
-
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class _02_TextUndoRedo implements KeyListener {
-	/* 
+	char currentchar;
+	String typedstr = "";
+	Character burrito;
+	JFrame frame = new JFrame("TextUndoRedo");
+	JPanel panel = new JPanel();
+	JLabel label = new JLabel();
+
+	Stack<String> undo = new Stack<String>();
+	/*
 	 * Create a JFrame with a JPanel and a JLabel.
 	 * 
-	 * Every time a key is pressed, add that character to the JLabel. It should look like a basic text editor.
+	 * Every time a key is pressed, add that character to the JLabel. It should look
+	 * like a basic text editor.
 	 * 
-	 * Make it so that every time the BACKSPACE key is pressed, the last character is erased from the JLabel.
-	 * Save that deleted character onto a Stack of Characters.
+	 * Make it so that every time the BACKSPACE key is pressed, the last character
+	 * is erased from the JLabel. Save that deleted character onto a Stack of
+	 * Characters.
 	 * 
-	 * Choose a key to be the Undo key. Make it so that when that key is pressed, the top Character is popped 
-	 * off the Stack and added back to the JLabel.
+	 * Choose a key to be the Undo key. Make it so that when that key is pressed,
+	 * the top Character is popped off the Stack and added back to the JLabel.
 	 * 
-	 * */
-	
-	
+	 */
+
 	public static void main(String[] args) {
 		_02_TextUndoRedo d = new _02_TextUndoRedo();
 	}
-	_02_TextUndoRedo (){
-		JFrame frame = new JFrame("TextUndoRedo");
-		JPanel panel = new JPanel();
-		JLabel label = new JLabel();
-		Stack<String> backspace=new Stack();
+
+	_02_TextUndoRedo() {
+
+		Stack<String> backspace = new Stack();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
+		frame.setSize(500, 500);
 		panel.setVisible(true);
-		panel.add(frame);
+		frame.add(panel);
+		panel.add(label);
 		frame.addKeyListener(this);
+
 	}
+
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
-		e.getKeyCode();
+		Character great = e.getKeyChar();
+		String greater = great.toString();
+		if (e.getKeyCode() == 8) {
+			// typedstr.charAt(typedstr.length()-1);
+			undo.push(greater);
+
+		}
 	}
+
 	@Override
 	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+		char kcode = e.getKeyChar();
+		typedstr += "" + kcode;
+		label.setText(typedstr);
+		if (e.getKeyCode() == 8) {
+			typedstr = typedstr.substring(typedstr.length() - 1);
+		}
 	}
+
 	@Override
 	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 }
