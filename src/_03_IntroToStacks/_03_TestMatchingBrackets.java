773f509bb76aba81f396cad29593ddfaa2bff222
blackbeltcam <league@WTS14.attlocal.net>
2019-05-03 16:30:39 -0700

    TestMatchingBrackets finish

--- src/_03_IntroToStacks/_03_TestMatchingBrackets.java
+++ src/_03_IntroToStacks/_03_TestMatchingBrackets.java
@@ -8,6 +8,7 @@ import java.util.Stack;
 import org.junit.Test;
 
 public class _03_TestMatchingBrackets {
+	Stack<Integer> bm = new Stack<Integer>();
 
 	@Test
 	public void testMatchingBrackets() {
@@ -18,9 +19,25 @@ public class _03_TestMatchingBrackets {
 		assertFalse(doBracketsMatch("}{"));
 	}
 
-	// USE A STACK TO COMPLETE THE METHOD FOR CHECKING IF EVERY OPENING BRACKET HAS A MATCHING CLOSING BRACKET
+	// USE A STACK TO COMPLETE THE METHOD FOR CHECKING IF EVERY OPENING BRACKET HAS
+	// A MATCHING CLOSING BRACKET
 	private boolean doBracketsMatch(String b) {
-		
+		bm.clear();
+		for (int uyir = 0; uyir < b.length(); uyir++) {
+
+			if (b.charAt(uyir) == '{') {
+				bm.push(uyir);
+			} else if (b.charAt(uyir) == '}') {
+				if (bm.size() == 0) {
+					return false;
+				}
+				bm.pop();
+			}
+
+		}
+		if (bm.size() == 0) {
+			return true;
+		}
 		return false;
 	}
 
