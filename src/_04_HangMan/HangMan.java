b99e4407bcbf1d0dea9b2a9c9d8ae34881f61107
blackbeltcam <league@WTS14.attlocal.net>
2019-05-03 16:56:07 -0700

    Hangman IP

--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -1,7 +1,17 @@
 package _04_HangMan;
 
-public class HangMan{
-	
-	
-	
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class HangMan {
+	JFrame frame = new JFrame("Hangman");
+	JPanel panel = new JPanel();
+	JTextField field = new JTextField();
+
+	public static void main(String[] args) {
+		String words = JOptionPane.showInputDialog("Wolcome to HANGMAN! \n How many words would you like to guess?");
+	}
+
 }
